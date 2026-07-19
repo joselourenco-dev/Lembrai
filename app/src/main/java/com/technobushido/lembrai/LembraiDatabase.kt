2aade9d5def6cd35f3c4be39c68a94b55b35907e
José Lourenco <josern1771@gmail.com>
2026-07-19 20:40:36 -0300

    Instanciando database

--- app/src/main/java/com/technobushido/lembrai/LembraiDatabase.kt
+++ app/src/main/java/com/technobushido/lembrai/LembraiDatabase.kt
@@ -1,9 +1,28 @@
 package com.technobushido.lembrai
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Pessoa::class], version = 1)
 abstract class LembraiDatabase : RoomDatabase(){
     abstract fun pessoaDao(): PessoaDao
+    companion object{
+        @Volatile
+        private var INSTANCE: LembraiDatabase? = null
+
+        fun getInstance(context: Context): LembraiDatabase{
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                context,
+                LembraiDatabase::class.java,
+                    "lembraidatabse"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+
+    }
 }
